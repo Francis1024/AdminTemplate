72b3ce2dbdc5743c249eb3092a6664ff51d3209b
zuotaiyu <yiyboy@qq.com>
2019-11-07 18:23:51 +0800

    first commit

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '后台管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description 是否固定Header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description 是否显示logo
+   */
+  sidebarLogo: true
+}
